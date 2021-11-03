d4ed26ec39a6527ed4e4ed3d96095c2ea3e319f2
saptarshide007 <81132512+saptarshide007@users.noreply.github.com>
2021-11-03 23:46:16 +0530

    Update Sidebar.js

--- src/components/recruitment-component/Sidebar.js
+++ src/components/recruitment-component/Sidebar.js
@@ -3,54 +3,87 @@ import { FaTimes, FaBars, FaMicrosoft, FaBahai } from "react-icons/fa";
 import styles from "./Sidebar.module.css";
 
 const Sidebar = (props) => {
-  const [showSideBar, setSideBar] = useState(false);
-  const toggleSideBar = () => {
-    setSideBar(!showSideBar);
-  };
+
+  let sideBarClassName = "example-side-bar";
+
+  if (props.expanded) {
+    sideBarClassName += " example-side-bar-expanded";
+  }
+
   return (
-    <div
-      className={`${styles["page-wrapper"]} ${styles["chiller-theme"]} ${
-        showSideBar && `${styles["toggled"]}`
-      }`}
-    >
-      <a
-        onClick={toggleSideBar}
-        id={`${styles["show-sidebar"]}`}
-        class="btn btn-sm btn-dark"
-        href="#"
-      >
-        <FaBars />
-      </a>
-      <nav id="sidebar" className={`${styles["sidebar-wrapper"]}`}>
-        <div className={`${styles["sidebar-content"]}`}>
-          <div className={`${styles["sidebar-brand"]}`}>
-            <a href="#">MENU</a>
-            <div id={`${styles["close-sidebar"]}`}>
-              <FaTimes onClick={toggleSideBar} />
-            </div>
-          </div>
-          <div className={`${styles["sidebar-menu"]}`}>
-            <ul>
-              {props.menuItems.map((menuItem) => {
-                return (
-                  <li className={`${styles["sidebar-dropdown"]}`}>
-                    <a href="#">
-                      <i>{getItemIcon(menuItem)}</i>
-                      <span>{menuItem}</span>
-                    </a>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+    <aside className={sideBarClassName}>
+      <div className="example-side-bar-title">
+        <div className="example-side-bar-title-icon">
         </div>
-      </nav>
-    </div>
+        <div className="example-side-bar-title-text">
+          <ExampleSkeleton size="1.5rem" color="#cfcfcf" chars="5" lines="1" />
+        </div>
+      </div>
+      <ul className="example-side-bar-list">
+        <ExampleSideBarLink chars="4" />
+        <ExampleSideBarLink chars="9" />
+        <ExampleSideBarLink chars="7" />
+        <ExampleSideBarLink chars="8" />
+        <ExampleSideBarLink chars="8" />
+        <ExampleSideBarLink chars="5" />
+        <ExampleSideBarLink chars="8" />
+        <ExampleSideBarLink chars="9" />
+        <ExampleSideBarLink chars="8" />
+        <ExampleSideBarLink chars="4" />
+      </ul>
+    </aside>
   );
 };
-const getItemIcon = (item) => {
-  if (item === "Dashboard") return <FaMicrosoft />;
-  else return <FaBahai />;
-};
+function ExampleIcon(props) {
+  const size = props.size;
+  const color = props.color;
+
+  return (
+    <div>
+    <i
+      className="example-icon"
+      style={{
+        "--size": size,
+        "--color": color,
+      }}
+    ><FaMicrosoft/></i>
+    
+    </div>
+  );
+}
+function ExampleSkeleton(props) {
+  const size = props.size;
+  const color = props.color;
+  const chars = props.chars;
+  const lines = props.lines;
+  const width = props.width;
+
+  return (
+    <div
+      className="example-skeleton"
+      style={{
+        "--size": size,
+        "--color": color,
+        "--chars": chars,
+        "--lines": lines,
+        width: width,
+      }}
+    ></div>
+  );
+}
+function ExampleSideBarLink(props) {
+  const chars = props.chars;
+
+  return (
+    <li>
+      <a href="#" className="example-side-bar-link" onClick={() => false}>
+        <span className="example-side-bar-link-icon">
+          <ExampleIcon size="16px" color="#f2f2f2" />
+        </span>
+        <ExampleSkeleton color="#f2f2f2" chars={chars} lines="1" />
+      </a>
+    </li>
+  );
+}
 
 export default Sidebar;
