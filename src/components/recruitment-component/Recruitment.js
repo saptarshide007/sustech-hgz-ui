6522ca2a8c2b8a1c19ddd1fc61833224bae9b80c
saptarshide007 <81132512+saptarshide007@users.noreply.github.com>
2021-11-10 23:59:09 +0530

    Update Recruitment.js

--- src/components/recruitment-component/Recruitment.js
+++ src/components/recruitment-component/Recruitment.js
@@ -1,30 +1,27 @@
-import "./Recruitment.css";
-import Sidebar from "./Sidebar";
+@ -3,6 +3,7 @@ import Sidebar from "./Sidebar";
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import Topbar from "./Topbar";
+import {Zoom,Fade,Roll} from 'react-reveal';
 
 const Recruitment = () => {
   const permissionList = [
-    "Dashboard",
-    "CreateJob",
-    "CreateJobPosition",
-    "CreateEmpoyee",
-  ];
+@ -14,14 +15,18 @@ const Recruitment = () => {
   const [expandOffcanvas, setExpandOffcanvas] = useState(false);
   return (
     <div className="example-offcanvas-container">
+      <Fade left>
       <Sidebar expanded={expandOffcanvas} permissionList={permissionList} />
+      </Fade>
       <div className="example-offcanvas-screen">
+      <Fade top>
         <Topbar
           hamburgerActive={expandOffcanvas}
           hamburgerClick={() => {
             setExpandOffcanvas(!expandOffcanvas);
           }}
         />
+        </Fade>
       </div>
     </div>
   );
-};
-
-export default Recruitment;
