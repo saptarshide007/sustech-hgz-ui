43f726fe0f4684a3a09e87e027c56f6ad586280e
saptarshide007 <saptarshide007@gmail.com>
2021-11-10 23:52:18 +0530

    u

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Recruitment from "./components/recruitment-component/Recruitment";
+import Requisition from "./components/recruitment-component/jobposition/Requisition";
 const App = () => {
-  return <Recruitment />;
+  return <Requisition />;
 };
 
 export default App;
---
+++ src/components/recruitment-component/jobposition/Requisition.js
@@ -0,0 +1,100 @@
+import {
+  FloatingLabel,
+  Form,
+  Row,
+  Col,
+  Button,
+  CloseButton,
+  Placeholder,
+} from "react-bootstrap";
+import styles from "./Requisition.module.css";
+const Requisition = () => {
+  return (
+    <div className={`${styles["form-wrapper"]}`}>
+      <div className={`${styles["form-card"]}`}>
+        <Form>
+          <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+            <Form.Label column sm={2}>
+              Position
+            </Form.Label>
+            <Col sm={10}>
+              <Form.Control type="text" placeholder="" />
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+            <Form.Label column sm={2}>
+              Type
+            </Form.Label>
+            <Col sm={10}>
+              <Form.Control type="text" placeholder="" />
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+            <Form.Label column sm={2}>
+              Skills
+            </Form.Label>
+            <Col sm={5}>
+              <Form.Control type="text" placeholder="" />
+            </Col>
+            <Col sm={5}>
+              <Button variant="success" size="md">
+                Add
+              </Button>
+            </Col>
+          </Form.Group>
+          <Form.Group
+            as={Row}
+            className="mb-3"
+            controlId="formHorizontalPassword"
+          >
+            <Form.Label column sm={2}>
+              Password
+            </Form.Label>
+            <Col sm={10}>
+              <Form.Control type="password" placeholder="Password" />
+            </Col>
+          </Form.Group>
+          <fieldset>
+            <Form.Group as={Row} className="mb-3">
+              <Form.Label as="legend" column sm={2}>
+                Radios
+              </Form.Label>
+              <Col sm={10}>
+                <Form.Check
+                  type="radio"
+                  label="first radio"
+                  name="formHorizontalRadios"
+                  id="formHorizontalRadios1"
+                />
+                <Form.Check
+                  type="radio"
+                  label="second radio"
+                  name="formHorizontalRadios"
+                  id="formHorizontalRadios2"
+                />
+                <Form.Check
+                  type="radio"
+                  label="third radio"
+                  name="formHorizontalRadios"
+                  id="formHorizontalRadios3"
+                />
+              </Col>
+            </Form.Group>
+          </fieldset>
+          <Form.Group as={Row} className="mb-3" controlId="formHorizontalCheck">
+            <Col sm={{ span: 10, offset: 2 }}>
+              <Form.Check label="Remember me" />
+            </Col>
+          </Form.Group>
+
+          <Form.Group as={Row} className="mb-3">
+            <Col sm={{ span: 10, offset: 2 }}>
+              <Button type="submit">Sign in</Button>
+            </Col>
+          </Form.Group>
+        </Form>
+      </div>
+    </div>
+  );
+};
+export default Requisition;
